448af68a47dc05a26c52df7ff21bc5f0d6a83349
unknown <sjosiasverabajana@gmail.com>
2025-11-26 20:13:19 -0500

    refactor: header y footer separados

--- includes/footer.php
+++ includes/footer.php
@@ -1,89 +1,125 @@
+<style>
+    footer {
+        background-color: #f2f2f2;
+        padding: 20px 0;
+        margin-top: 40px;
+    }
+
+    #contenedorFooter {
+        width: 80%;
+        margin: auto;
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        grid-template-rows: auto auto;
+        gap: 30px;
+        align-items: start;
+    }
+
+    .redes {
+        text-align: center;
+        margin-top: 20px;
+    }
+
+    .redes a {
+        margin: 0 10px;
+        display: inline-block;
+    }
+
+    .informacionContacto {
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+    }
+
+    .informacionContacto .contacto-linea {
+        display: flex;
+        align-items: center;
+    }
+
+    .informacionContacto img {
+        margin-right: 10px;
+        flex-shrink: 0;
+    }
+
+    .informacionContacto p {
+        margin: 0;
+        padding: 0;
+    }
+
+    .acercaDe ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    .acercaDe li {
+        margin: 5px 0;
+    }
+
+    .formularioFooter {
+        text-align: left;
+        margin-top: 0;
+    }
+
+    .formularioFooter .campo {
+        margin: 10px 0;
+    }
+
+    .formularioFooter input[type="email"] {
+        padding: 8px;
+        width: 180px;
+        margin-right: 10px;
+    }
+
+    .formularioFooter button {
+        padding: 8px 16px;
+    }
+</style>
+
 <footer>
-            <div id="contenedorFooter">
+    <div id="contenedorFooter">
 
-                <div class="informacionContacto">
-
-                    <h3>Información de Contacto</h3>
-
-                    <div class="contacto-linea">
-                        <img src="https://img.icons8.com/?size=100&id=3723&format=png&color=000000" width="20px"
-                            height="20px" alt="Icono de ubicación">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                    </div>
-
-                    <div class="contacto-linea">
-                        <img src="https://img.icons8.com/?size=100&id=9659&format=png&color=000000" width="20px"
-                            height="20px" alt="Icono de teléfono">
-                        <p>+(593) 99 999 9999</p>
-                    </div>
-
-                    <div class="contacto-linea">
-                        <img src="https://img.icons8.com/?size=100&id=12580&format=png&color=000000" width="20px"
-                            height="20px" alt="Icono de email">
-                        <p>salon.eventos@gmail.com</p>
-                    </div>
-                </div>
-
-                <div class="acercaDe">
-                    <h3>Acerca de Nosotros</h3>
-                    <ul>
-                        <li>Nuestra infraestructura</li>
-                        <li>Equipo</li>
-                        <li>Trabaja con nosotros</li>
-                        <li>Políticas de privacidad</li>
-                        <li>Términos y condiciones</li>
-                    </ul>
-
-                </div>
-
-                <div class="formularioFooter">
-
-                    <h3>Suscribete para más novedades</h3>
-
-                    <form class="form">
-
-                        <div class="campo">
-                            <input type="email" placeholder="Ingresa tu correo electrónico">
-                            <button type="submit">Suscribirse</button>
-                        </div>
-
-                        <div class="campo">
-                            <input type="checkbox" id="terminos" name="terminos">
-                            <label for="terminos">Acepto los términos y condiciones</label>
-                        </div>
-
-                        <div class="campo">
-                            <input type="checkbox" id="privacidad" name="privacidad">
-                            <label for="privacidad">He leído la política de privacidad</label>
-                        </div>
-
-                    </form>
-
-                </div>
-
+        <div class="informacionContacto">
+            <h3>Contáctanos</h3>
+            <div class="contacto-linea">
+                <img src="img/footer/phone.png" width="20">
+                <p>+593 999 999 999</p>
             </div>
 
-            <div class="redes">
-                <p>Siguenos en nuestras redes: </p>
-
-                <a href="https://www.facebook.com">
-                    <img src="https://img.icons8.com/?size=100&id=118497&format=png&color=000000"
-                        alt="Icono de Facebook" width="45px" height="45px">
-                </a>
-                <a href="https://www.instagram.com">
-                    <img src="https://img.icons8.com/?size=100&id=ZRiAFreol5mE&format=png&color=000000"
-                        alt="Icono de Instagram" width="45px" height="45px">
-                </a>
-                <a href="https://www.x.com">
-                    <img src="https://img.icons8.com/?size=100&id=phOKFKYpe00C&format=png&color=000000" alt="Icono de X"
-                        width="45px" height="45px">
-                </a>
+            <div class="contacto-linea">
+                <img src="img/footer/email.png" width="20">
+                <p>info@salones.com</p>
             </div>
 
-        </footer>
+            <div class="contacto-linea">
+                <img src="img/footer/location.png" width="20">
+                <p>Quito, Ecuador</p>
+            </div>
+        </div>
 
+        <div class="acercaDe">
+            <h3>Acerca de</h3>
+            <ul>
+                <li><a href="">Quiénes somos</a></li>
+                <li><a href="">Términos y condiciones</a></li>
+                <li><a href="">Políticas de privacidad</a></li>
+            </ul>
+        </div>
+
+        <div class="formularioFooter">
+            <h3>Suscríbete</h3>
+            <div class="campo">
+                <input type="email" placeholder="Correo electrónico">
+                <button>Enviar</button>
+            </div>
+        </div>
     </div>
 
-</body>
+    <div class="redes">
+        <a href="#"><img src="img/footer/facebook.png" width="25"></a>
+        <a href="#"><img src="img/footer/instagram.png" width="25"></a>
+        <a href="#"><img src="img/footer/twitter.png" width="25"></a>
+    </div>
+</footer>
 
+</body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -8,7 +8,6 @@
     <title>Document</title>
 
     <style>
-        /* MANTENEMOS TODO EL CSS EN EL HEADER.PHP PORQUE ESTABA EN LÍNEA */
         .contenedorPrincipal {
             width: auto;
             padding: 10px;
@@ -37,209 +36,27 @@
         #botones button {
             margin-left: 10px;
         }
-
-        #contenedorImagen {
-            margin-top: 20px;
-            height: 450px;
-            padding: 20px;
-            /* Asegúrate de que esta ruta sea correcta para tu imagen */
-            background: url(img/index/salon.jpg) center/cover no-repeat; 
-
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-        }
-
-        #contenedorImagen>h1 {
-            color: white;
-            font-size: 48px;
-            margin-top: 100px;
-            text-align: center;
-        }
-
-        #contenedorImagen p {
-            color: white;
-            font-size: 24px;
-            text-align: center;
-        }
-
-        #sobreNosotros {
-            margin: auto;
-            width: 50%;
-        }
-
-        #sobreNosotros h2 {
-            text-align: center;
-            margin-bottom: 20px;
-        }
-
-        #sobreNosotros p {
-            line-height: 1.30;
-        }
-
-
-        .imagenDerecha {
-            width: 300px;
-            float: right;
-            margin: 0;
-        }
-
-        #end {
-            clear: both;
-            margin: 0;
-        }
-
-        #seccionBeneficios,
-        #salonesContenedor {
-            text-align: center;
-            margin: 40px auto;
-            width: 80%;
-        }
-
-        .items,
-        .salones {
-            display: flex;
-            justify-content: center;
-            gap: 5%;
-        }
-
-        .items div,
-        .salones div {
-            flex: 250px;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            text-align: center;
-            justify-content: space-between;
-        }
-
-        .items img {
-            width: 60px;
-            margin-bottom: 5px;
-        }
-
-        .salones img {
-            width: 250px;
-            margin-bottom: 5px;
-        }
-
-        .salones a {
-            text-decoration: none;
-            color: blue;
-        }
-
-        .salones a:hover {
-            text-decoration: underline;
-        }
-
-        .salones h3,
-        p,
-        a {
-            margin: 5px;
-            padding: 3px;
-        }
-
-        footer {
-            background-color: #f2f2f2;
-            padding: 20px 0;
-            margin-top: 40px;
-        }
-
-        #contenedorFooter {
-            width: 80%;
-            margin: auto;
-            display: grid;
-            grid-template-columns: 1fr 1fr 1fr;
-            grid-template-rows: auto auto;
-            gap: 30px;
-            align-items: start;
-            align-self: self-start;
-        }
-
-        .redes {
-            text-align: center;
-            margin-top: 20px;
-        }
-
-        .redes a {
-            margin: 0 10px;
-            display: inline-block;
-        }
-
-        .informacionContacto {
-            display: flex;
-            flex-direction: column;
-            gap: 10px;
-        }
-
-        .informacionContacto .contacto-linea {
-            display: flex;
-            align-items: center;
-        }
-
-        .informacionContacto img {
-            margin-right: 10px;
-            flex-shrink: 0;
-        }
-
-        .informacionContacto p {
-            margin: 0;
-            padding: 0;
-        }
-
-        .acercaDe ul {
-            list-style-type: none;
-            padding: 0;
-        }
-
-        .acercaDe li {
-            margin: 5px 0;
-        }
-
-        .formularioFooter {
-            text-align: left;
-            margin-top: 0;
-        }
-
-        .formularioFooter .campo {
-            margin: 10px 0;
-        }
-
-        .formularioFooter input[type="email"] {
-            padding: 8px;
-            width: 180px;
-            margin-right: 10px;
-        }
-
-        .formularioFooter button {
-            padding: 8px 16px;
-        }
     </style>
 </head>
 
 <body>
-
     <div class="contenedorPrincipal">
 
         <div id="contenedorHeader">
-
             <img id="logo" src="" alt="Imagen del logo">
 
             <div id="navegacion">
                 <nav>
-                    <a href="#.html">Inicio</a>
+                    <a href="index.php">Inicio</a>
                     <a href="#.html">Salones</a>
                     <a href="#.html">Servicios</a>
                     <a href="#.html">Sobre Nosotros</a>
                     <a href="#.html">Contáctenos</a>
                 </nav>
-
             </div>
 
             <div id="botones">
                 <button>Registrar</button>
                 <button>Iniciar sesión</button>
             </div>
-
         </div>
