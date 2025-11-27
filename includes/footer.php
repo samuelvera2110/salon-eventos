6479c00a34a05523969b14bf380ce2a79797c538
unknown <sjosiasverabajana@gmail.com>
2025-11-26 19:52:04 -0500

    Estructura del proyecto

---
+++ includes/footer.php
@@ -0,0 +1,89 @@
+<footer>
+            <div id="contenedorFooter">
+
+                <div class="informacionContacto">
+
+                    <h3>Información de Contacto</h3>
+
+                    <div class="contacto-linea">
+                        <img src="https://img.icons8.com/?size=100&id=3723&format=png&color=000000" width="20px"
+                            height="20px" alt="Icono de ubicación">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+
+                    <div class="contacto-linea">
+                        <img src="https://img.icons8.com/?size=100&id=9659&format=png&color=000000" width="20px"
+                            height="20px" alt="Icono de teléfono">
+                        <p>+(593) 99 999 9999</p>
+                    </div>
+
+                    <div class="contacto-linea">
+                        <img src="https://img.icons8.com/?size=100&id=12580&format=png&color=000000" width="20px"
+                            height="20px" alt="Icono de email">
+                        <p>salon.eventos@gmail.com</p>
+                    </div>
+                </div>
+
+                <div class="acercaDe">
+                    <h3>Acerca de Nosotros</h3>
+                    <ul>
+                        <li>Nuestra infraestructura</li>
+                        <li>Equipo</li>
+                        <li>Trabaja con nosotros</li>
+                        <li>Políticas de privacidad</li>
+                        <li>Términos y condiciones</li>
+                    </ul>
+
+                </div>
+
+                <div class="formularioFooter">
+
+                    <h3>Suscribete para más novedades</h3>
+
+                    <form class="form">
+
+                        <div class="campo">
+                            <input type="email" placeholder="Ingresa tu correo electrónico">
+                            <button type="submit">Suscribirse</button>
+                        </div>
+
+                        <div class="campo">
+                            <input type="checkbox" id="terminos" name="terminos">
+                            <label for="terminos">Acepto los términos y condiciones</label>
+                        </div>
+
+                        <div class="campo">
+                            <input type="checkbox" id="privacidad" name="privacidad">
+                            <label for="privacidad">He leído la política de privacidad</label>
+                        </div>
+
+                    </form>
+
+                </div>
+
+            </div>
+
+            <div class="redes">
+                <p>Siguenos en nuestras redes: </p>
+
+                <a href="https://www.facebook.com">
+                    <img src="https://img.icons8.com/?size=100&id=118497&format=png&color=000000"
+                        alt="Icono de Facebook" width="45px" height="45px">
+                </a>
+                <a href="https://www.instagram.com">
+                    <img src="https://img.icons8.com/?size=100&id=ZRiAFreol5mE&format=png&color=000000"
+                        alt="Icono de Instagram" width="45px" height="45px">
+                </a>
+                <a href="https://www.x.com">
+                    <img src="https://img.icons8.com/?size=100&id=phOKFKYpe00C&format=png&color=000000" alt="Icono de X"
+                        width="45px" height="45px">
+                </a>
+            </div>
+
+        </footer>
+
+    </div>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,94 @@
+<?php 
+    require 'includes/header.php';
+?>
+
+        <div id="contenedorImagen">
+            <h1><b>Tu evento</b></h1>
+            <p><strong>Celebra momentos inolvidables en nuestros salones</strong></p>
+            <button>Reserva ahora</button>
+        </div>
+
+        <div id="sobreNosotros">
+            <h2>Sobre Nosotros</h2>
+
+            <img src="img/index/salon-bodas.jpeg" class="imagenDerecha">
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, officia sapiente omnis totam quo
+                officiis eius cumque ullam sequi quasi, fugit, aperiam ipsam suscipit neque optio maiores hic dicta aut.
+            </p>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum magnam fuga aliquam, ipsam voluptatum
+                beatae laborum illum earum, libero quo minus provident quae illo id vero cumque eveniet labore pariatur!
+            </p>
+        </div>
+
+        <div id="end"></div>
+
+        <div id="seccionBeneficios">
+            <h2>¿Por qué elegirnos para tu próximo evento?</h2>
+
+            <div class="items">
+
+                <div>
+                    <img src="img/index/reserva.png">
+                    <h3>Reserva en tiempo real</h3>
+                    <p>Realiza tu reserva al instante, sin esperas ni llamadas</p>
+                </div>
+
+                <div>
+                    <img src="img/index/servicio.png">
+                    <h3>Servicios</h3>
+                    <p>Ofrecemos una variedad de servicios para hacer tu evento especial</p>
+                </div>
+
+                <div>
+                    <img src="img/index/darse-la-mano.png">
+                    <h3>Asesorías personalizadas</h3>
+                    <p>Nuestro equipo brinda soporte para garantizar cada detalle de tu evento</p>
+                </div>
+
+            </div>
+
+        </div>
+
+        <div id="salonesContenedor">
+
+            <h2>Nuestros salones destacados</h2>
+
+            <div class="salones">
+
+                <div>
+                    <img src="img/index/salon-bodas.jpeg">
+                    <h3>Salón Elegance</h3>
+                    <p>Espacio ideal para bodas y eventos corporativos.</p>
+                    <a href="">Ver más</a>
+                </div>
+
+                <div>
+                    <img src="img/index/salon-fiesta-infantil.jpg">
+                    <h3>Salón Fiesta</h3>
+                    <p>Perfecto para celebraciones familiares y fiestas privadas.</p>
+                    <a href="">Ver más</a>
+                </div>
+
+                <div>
+                    <img src="img/index/salon-conferencias.jpg">
+                    <h3>Salón Business</h3>
+                    <p>Espacio moderno para conferencias y reuniones de negocios.</p>
+                    <a href="">Ver más</a>
+                </div>
+
+                <div>
+                    <img src="img/index/salon-terraza.jpeg">
+                    <h3>Salón Terraza</h3>
+                    <p>Espacio al aire libre, ideal para cocteles y recepciones.</p>
+                    <a href="">Ver más</a>
+                </div>
+
+            </div>
+
+        </div>
+
+<?php
+    require 'includes/footer.php';
+?>
