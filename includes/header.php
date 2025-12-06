ee68c3aa66666b715a2a7930b7ad026d8c846daa
unknown <sjosiasverabajana@gmail.com>
2025-12-06 18:53:03 -0500

    Agregado estilos.css, pagina index, paginaRegistro

--- includes/header.php
+++ includes/header.php
@@ -1,62 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="Samuel Vera">
-    <title>Document</title>
-
-    <style>
-        .contenedorPrincipal {
-            width: auto;
-            padding: 10px;
-            margin: auto;
-        }
-
-        #contenedorHeader {
-            display: flex;
-            justify-content: space-between;
-            padding: 0 20px;
-            align-items: center;
-            border-bottom: 1px solid lightgray;
-        }
-
-        #logo {
-            width: 100px;
-        }
-
-        #navegacion nav a {
-            text-decoration: none;
-            padding: 20px;
-            font-size: 14px;
-            display: inline-block;
-        }
-
-        #botones button {
-            margin-left: 10px;
-        }
-    </style>
+    <title>Eventix</title>
+    <link rel="stylesheet" href="css/estilos.css"/>
 </head>
-
 <body>
-    <div class="contenedorPrincipal">
-
-        <div id="contenedorHeader">
-            <img id="logo" src="" alt="Imagen del logo">
-
-            <div id="navegacion">
+    <header>
+        <div class="menuNavegacion">
+            <div class="logo">
+                <img src="img/index/logo.jpeg" alt="Imagen del logo" style="height: 70px; width: auto;object-fit: contain;">
+            </div>
+            <div class="menu">
                 <nav>
                     <a href="index.php">Inicio</a>
-                    <a href="#.html">Salones</a>
-                    <a href="#.html">Servicios</a>
-                    <a href="#.html">Sobre Nosotros</a>
-                    <a href="#.html">Contáctenos</a>
+                    <a href="">Salones</a>
+                    <a href="">Servicios</a>
+                    <a href="">Sobre Nosotros</a>
+                    <a href="">Contáctenos</a>
+                    <a href="">Reservaciones</a>
                 </nav>
             </div>
-
-            <div id="botones">
-                <button>Registrar</button>
-                <button>Iniciar sesión</button>
+            <div class="botones">
+                <button onclick="window.location.href='registro.php'">Registrate</button>
             </div>
         </div>
+    </header>
